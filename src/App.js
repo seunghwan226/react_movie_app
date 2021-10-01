6ae9503efa2bb94569667cbf6e4c5bc119757e1f
dnlengnl12 <70835908+dnlengnl12@users.noreply.github.com>
2021-10-01 22:29:16 +0900

    Learn props, jsx, propTypes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from "react";
-import Potato from "./potato"
+import PropTypes from "prop-types";
+
+function Food({name, rating}) {
+  console.log(name);
+  return <div>
+      <h1>i like {name}</h1>
+      <h4>{rating}/ 5.0</h4>
+    </div>
+}
+
+const foodILike = [{
+  id:1,
+  name: "kimchi",
+  rating: 5
+},
+{
+  id:2,
+  name: "bibimbob",
+  rating: 4.9
+}];
+
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
+function renderFood(dish) {
+  console.log(dish);
+  return <Food key={dish.id} name={dish.name} rating ={dish.rating}></Food>
+}
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato />
+      {foodILike.map(renderFood)}
     </div>
   )
 }
