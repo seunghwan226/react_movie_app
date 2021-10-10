b8159d461041cbd419c15ffdb9850b32ddb3b658
lsh <dnlengnl12@gmail.com>
2021-10-10 20:09:21 +0900

    Created Detail.css

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
     componentDidMount() {
@@ -10,8 +11,23 @@ class Detail extends React.Component {
     }
     render() {
         const {location} = this.props;
+        const {poster} = this.props.location.state;
         if(location.state) {
-            return <span>{location.state.title}</span>;
+            return (
+                <div className="movie-detail">
+                    <div className="movie-detail-background">
+                        <img src={poster}/>
+                    </div>
+                    <div className="movie-detail-header">
+                        <div className="movie-detail-header__img">
+                            <img src={poster}/>
+                        </div>
+                        <div className="movie-detail-header__title">
+                            <h2>{location.state.title}</h2>
+                        </div>
+                    </div>
+                </div>
+            )
         } else {
             return null;
         }
